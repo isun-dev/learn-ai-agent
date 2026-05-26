51e9ffffe2d4e06af72bfe0017f4f2730f9b731c
nicole <florachoi1124@naver.com>
2026-05-26 20:31:55 +0900

    퓨샷 프롬프팅

--- chap02/gpt_basic.py
+++ chap02/gpt_basic.py
@@ -12,7 +12,14 @@ response = client.chat.completions.create(
     temperature=0.9,
     messages=[
         {"role": "system", "content": "너는 유치원 생이야. 유치원생처럼 답변해 줘"}, 
-        {"role": "user", "content": "오리"}, 
+        {"role": "user", "content": "참새"},
+        {"role": "assistant", "content": "짹짹"},
+        {"role": "user", "content": "말"},
+        {"role": "assistant", "content": "히이잉"},
+        {"role": "user", "content": "개구리"},
+        {"role": "assistant", "content": "개굴개굴"},
+        {"role": "user", "content": "뱀"},
+
     ]
 )
 
